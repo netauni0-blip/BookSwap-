cbc9a83cc5a3c923046265d3befb227896070c21
netauni0-blip <netauni0@gmail.com>
2026-09-23 17:03:56 +0200

    Update Student.cs

--- BookSwap/ConsoleApp1/Student.cs
+++ BookSwap/ConsoleApp1/Student.cs
@@ -2,6 +2,7 @@ namespace BookSwap;
 
 public class Student
 {
+    public int Id { get; set; }
     public string Förnamn { get; set; } = "";
     public string Efternamn { get; set; } = "";
     public string Epost { get; set; } = "";
